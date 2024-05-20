e433b1acca27df33c039c8c3f23b25ba78f6cb62
vishalgit2005 <vishalsandakelum20@gmail.com>
2024-05-20 15:41:27 +0530

    create supplier save & get function

--- ajax/supplier/Supplier.js
+++ ajax/supplier/Supplier.js
@@ -1,1 +1,73 @@
 let supplierURI = 'http://localhost:8080/app/api/v0/suppliers'
+
+$('.supplierdatasave').click(function(){
+    const supplierData = getAllSupplierDataFromField();
+    $.ajax({
+        url:supplierURI,
+        method:'POST',
+        data:JSON.stringify(supplierData),
+        contentType: 'application/json',
+        headers: {
+            'Authorization': 'Bearer ' + bearerToken
+        },
+    
+        success: function(resp){
+            showAlert("success","Success","Supplier "+resp.supplierCode+" Saved Sucessfully.");
+            clearAllSupplierField();
+        },
+        error:function(resp){
+        }
+    });
+});
+
+$('.supplierdataget').click(function(){
+    $.ajax({
+        url:(supplierURI+'/'+$('.suppliercode').val()),
+        method:'PATCH',
+        contentType: 'application/json',
+        headers: {
+            'Authorization': 'Bearer ' + bearerToken
+        },
+    
+        success: function(resp){
+            clearAllSupplierField();
+            $('.suppliercode').val(resp.supplierCode),
+            $('.suppliername').val(resp.supplierName),
+            $('.suppliercategory option').each(function() {
+                if ($(this).text() === resp.category) {
+                    $(this).prop('selected', true);
+                }
+            }),
+            $('.supplieraddress01').val(resp.addressLine01),
+            $('.supplieraddress02').val(resp.addressLine02),
+            $('.supplieraddress03').val(resp.addressLine03),
+            $('.supplieraddress04').val(resp.addressLine04),
+            $('.supplieraddress05').val(resp.addressLine05),
+            $('.supplieraddress06').val(resp.addressLine06),
+            $('.suppliercontactnumber').val(resp.contactNo1),
+            $('.supplierlandlinenumber').val(resp.landLineNo),
+            $('.supplieremail').val(resp.email)
+        },
+        error:function(resp){
+            showAlert("error","Oops",resp.message);
+            clearAllSupplierField();
+        }
+    });
+});
+
+function getAllSupplierDataFromField(){
+    return{
+        supplierCode: $('.suppliercode').val(),
+        supplierName: $('.suppliername').val(),
+        category: $('.suppliercategory').find('option:selected').text(),
+        addressLine01: $('.supplieraddress01').val(),
+        addressLine02: $('.supplieraddress02').val(),
+        addressLine03: $('.supplieraddress03').val(),
+        addressLine04: $('.supplieraddress04').val(),
+        addressLine05: $('.supplieraddress05').val(),
+        addressLine06: $('.supplieraddress06').val(),
+        contactNo1: $('.suppliercontactnumber').val(),
+        landLineNo: $('.supplierlandlinenumber').val(),
+        email: $('.supplieremail').val()
+    }
+}
