3ac03ca16eecdc89eb7348d15dc9d0b5ce949a03
vishalgit2005 <vishalsandakelum20@gmail.com>
2024-05-20 17:36:26 +0530

    create user save function

--- ajax/user/User.js
+++ ajax/user/User.js
@@ -1,1 +1,29 @@
 let userURI = 'http://localhost:8080/app/api/v0/user'
+
+$('.userdatasave').click(function(){
+    const userData = getAllUserDataFromField();
+    $.ajax({
+        url:userURI,
+        method:'POST',
+        data:JSON.stringify(userData),
+        contentType: 'application/json',
+        headers: {
+            'Authorization': 'Bearer ' + bearerToken
+        },
+    
+        success: function(resp){
+            showAlert("success","Success","User "+resp.email+" Saved Sucessfully.");
+            clearAllUserField();
+        },
+        error:function(resp){
+        }
+    });
+});
+
+function getAllUserDataFromField(){
+    return{
+        email: $('.useremail').val(),
+        password: $('.userpassword').val(),
+        role: $('.userrole').find('option:selected').text(),
+    }
+}
