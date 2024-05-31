4f4d52e238a926413a319e6afc4b2dce9d30c4c8
vishalgit2005 <vishalsandakelum20@gmail.com>
2024-05-31 21:10:54 +0530

    create signup form

--- ajax/login/Login.js
+++ ajax/login/Login.js
@@ -1,4 +1,5 @@
 let signInURI = 'http://localhost:8080/app/api/v0/auth/signin';
+let signUpURI = 'http://localhost:8080/app/api/v0/auth/signup';
 let token;
 
 $('.signinbtn').click(function(){
@@ -21,9 +22,37 @@ $('.signinbtn').click(function(){
     });
 });
 
+$('.signupbtn').click(function(){
+    const signUpData = getAllSignUpDataFromField();
+    $.ajax({
+        url:signUpURI,
+        method:'POST',
+        data:JSON.stringify(signUpData),
+        contentType: 'application/json',
+    
+        success: function(resp){
+            token = resp.token;
+            console.log(token);
+            localStorage.setItem('authToken', token);
+            window.location.href = 'index.html';
+        },
+        error:function(resp){
+            showAlert("error","Oops","Please Check & Enter Correct Email Password");
+        }
+    });
+});
+
 function getAllSignInDataFromField(){
     return{
         email:$('.usernamefield').val(),
         password:$('.passwordfield').val()
+    }
+}
+
+function getAllSignUpDataFromField(){
+    return{
+        email:$('.signupusernamefield').val(),
+        password:$('.signuppasswordfield').val(),
+        role:$('.signuproleselected').find('option:selected').text()
     }
 }
